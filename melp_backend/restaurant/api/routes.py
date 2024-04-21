bf2f391a32f9e80326e68897041f52c718c760cf
christianLeon <chr.leon.y@gmail.com>
2024-04-20 22:32:35 -0600

    routes names added

--- melp_backend/restaurant/api/routes.py
+++ melp_backend/restaurant/api/routes.py
@@ -5,9 +5,9 @@ from restaurant.api.views import RestaurantViewSet;
 app_name="restaurant"
 
 urlpatterns = [
-    path('', RestaurantViewSet.as_view({ 'post':'post' }),name='createRestaurant'),
+    path('', RestaurantViewSet.as_view({ 'post':'post' }),name='postRestaurant'),
     path('<str:pk>/', RestaurantViewSet.as_view({ 'get':'get' }),name='getRestaurant'),
-    path('<str:pk>/', RestaurantViewSet.as_view({ 'put':'put' }),name='updateRestaurant'),
+    path('<str:pk>/', RestaurantViewSet.as_view({ 'put':'put' }),name='putRestaurant'),
     path('<str:pk>/', RestaurantViewSet.as_view({ 'delete':'delete' }),name='deleteRestaurant'),
     path('import/', RestaurantViewSet.as_view({ 'post':'importCsv' }))
 ]
